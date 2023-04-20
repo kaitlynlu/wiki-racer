4be0c7055a9e4d88525618a6e190caf1c4f6cd3e
kaitlynlu <lu.kaitlyn@gmail.com>
2023-04-19 23:05:39 -0400

    added graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,39 @@
-class Graph {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+    HashMap<String, ArrayList<String>> graph;
+
+    public Graph(int n) {
+        graph = new HashMap<>();
+
+    }
+
+
+    public boolean hasEdge(String u, String v) {
+        if (this.graph.containsKey(u)) {
+            return this.graph.get(u).contains(v);
+        }
+        return false;
+    }
+
+
+    public void addEdge(String u, String v) {
+        if (this.graph.containsKey(u) && !hasEdge(u,v)) {
+            this.graph.get(u).add(v);
+        } else {
+            this.graph.put(u, new ArrayList<>());
+            this.graph.get(u).add(v);
+        }
+
+    }
+
+
+    public ArrayList<String> getNeighbors(String u) {
+        if (!graph.containsKey(u)) {
+            return new ArrayList<>();
+        }
+        return graph.get(u);
     }
 
 }
